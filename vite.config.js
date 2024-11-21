ebaded8bbdf36230cd533beb6b931d074225b55e
Lorens Osman <lorens.osman.dev@gmail.com>
2024-11-21 07:47:43 +0300

    add: vite --mode to separate development from production

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,28 @@
 import { defineConfig } from "vite";
 import path from "node:path";
 
-export default defineConfig({
-	publicDir: "public", // Vite will copy all files from this directory to the output including manifest.json
+export default defineConfig(({ mode }) => {
+	/* The mode parameter corresponds to the value passed with --mode during build.
+ Vite defaults to development in dev and production in build (vite build or vite build --mode production).*/
+	const isProduction = mode === "production";
 
-	build: {
-		rollupOptions: {
-			input: path.resolve(__dirname, "src/theme.scss"),
+	return {
+		publicDir: "public", // Vite will copy all files from this directory to the output including manifest.json
 
-			output: {
-				assetFileNames: "theme.css",
+		build: {
+			rollupOptions: {
+				input: path.resolve(__dirname, "src/theme.scss"),
+				output: {
+					assetFileNames: "theme.css",
+				},
 			},
+
+			minify: isProduction, // Enable minification in production
+			cssMinify: isProduction, // Minify CSS only in production
+			outDir: "star", // Sets the output directory for the CSS file
+			emptyOutDir: true,
+
+			cssTarget: "esnext",
 		},
-
-		minify: false, // Disable all minification
-		cssMinify: false, // Specifically disable CSS minification
-		outDir: "star", // Sets the output directory for the CSS file
-		emptyOutDir: true,
-
-		cssTarget: "esnext",
-	},
+	};
 });
